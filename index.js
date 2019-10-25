17975aafa09685bf2b888b3bd530ee4a88643f63
Muhammed Yavuz <muhametyavuz@hotmail.com>
2019-10-25 10:12:15 +0300

    fix cors

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ mongoose
   .catch(err => console.log("error:", err));
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/movies", movieRoutes);
 app.use("/actors", actorRoutes);
-app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
